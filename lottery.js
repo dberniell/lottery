6391fd1f2335e32b497336ed2b1d3fd895fce84f
dberniell <davidberniell@gmail.com>
2014-10-09 12:30:20 +0000

    use a variable to display magic number

--- lottery.js
+++ lottery.js
@@ -10,7 +10,7 @@ var calculateResult = function (whiteBalls, powerBall){
 	}
 	return 0;
 }
-
+var luckyNumber = 7;
 var lottery = function (){
 	var powerBall, whiteBalls, index, result;
 	if(process.argv.length !== 8){
@@ -23,7 +23,7 @@ var lottery = function (){
 	   whiteBalls[index] = parseInt(process.argv[2+index]);
 	}
 	result = calculateResult(whiteBalls, powerBall);
-	if (powerBall === 7) {
+	if (powerBall === luckyNumber) {
 			result = result * 2;
 	};
 	console.log(result + ' percent chance of winning.');
